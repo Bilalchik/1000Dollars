24f0870c510f6665730257323c011ff8f4c57b13
Yrysgyl <164658092+Yrysgyl@users.noreply.github.com>
2025-04-10 01:15:59 +0600

    first commit

--- core/product/serializers.py
+++ core/product/serializers.py
@@ -17,7 +17,41 @@ class BrandListSerializer(serializers.ModelSerializer):
 
 
 class ProductListSerializer(serializers.ModelSerializer):
+    main_cover = serializers.ImageField(use_url=True)
+    brand = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_brand(self, obj):
+        return obj.brand.title  
+
+    def get_category(self, obj):
+        return obj.category.title
+    
+    def get_status(self, obj):
+        return obj.get_status_display()
+    
+    
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Стоимость не может быть отрицательной.")
+        return value
+
+    def validate_discount_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Стоимость с учетом скидки не может быть отрицательной.")
+        return value
+
+    def validate(self, attrs):
+        price = attrs.get('price')
+        discount_price = attrs.get('discount_price')
+
+        if discount_price > price:
+            raise serializers.ValidationError("Стоимость с учетом скидки не может быть больше основной стоимости.")
+        
+        return attrs
 
