524defc41678a62b019d00dde95d52f5e2b46bf9
jenishdatka <jenishdatka@gmail.com>
2025-04-10 17:16:55 +0600

    changed status, discount price, validate

--- core/product/serializers.py
+++ core/product/serializers.py
@@ -1,3 +1,4 @@
+import data
 from django.contrib.admin.utils import model_ngettext
 from rest_framework import serializers
 from .models import Product, Banner, Brand
@@ -11,13 +12,33 @@ class BannerListSerializer(serializers.ModelSerializer):
 
 
 class BrandListSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+
     class Meta:
         model = Brand
         fields = '__all__'
 
 
 class ProductListSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, obj):
+        return obj.get_status_display()
+
     class Meta:
         model = Product
         fields = '__all__'
 
+    def get_status(self, obj):
+        return obj.get_status_display()
+
+    def validate(self, data):
+        price = data.get('price')
+        discount_price = data.get('discount_price')
+
+        if discount_price > price:
+            raise serializers.ValidationError({
+                'discount_price': 'Цена со скидкой не может быть выше обычной цены.'
+            })
+
+        return data
