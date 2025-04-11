d3eb52799b4d99034ef3d29d9a778a453d278904
jenishdatka <jenishdatka@gmail.com>
2025-04-11 16:19:46 +0600

    MyUserRegistration

--- core/user/views.py
+++ core/user/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import UserRegisterSerializer
 
 class MyUserRegisterView(APIView):
-
-    def get(self, request):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': 'Пользователь успешно зарегистрирован!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
