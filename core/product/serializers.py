9440730572a904139a7a50391c0a25c01b6d6e8c
Aizhan <osrarova.i@icloud.com>
2025-04-12 13:12:03 +0600

    git pull mo rebase

--- core/product/serializers.py
+++ core/product/serializers.py
@@ -1,5 +1,5 @@
-from django.contrib.admin.utils import model_ngettext
 from rest_framework import serializers
+
 from .models import Product, Banner, Brand
 
 
@@ -17,18 +17,10 @@ class BrandListSerializer(serializers.ModelSerializer):
 
 
 class ProductListSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
     brand = serializers.SlugRelatedField(read_only=True, slug_field='title')
     category = serializers.SlugRelatedField(read_only=True, slug_field='title')
     status = serializers.SerializerMethodField()
-=======
->>>>>>> 871fa30793a831446398473267ee95216ebe327a
 
     class Meta:
         model = Product
         fields = ('id', 'title', 'category', 'price', 'discount_price', 'main_cover', 'get_status_display')
-
-
-    def get_status(self, obj):
-        return obj.get_status_display()
-
